58c4270873a88fb29bf13af5593df49d68ad8736
druthi0723 <druthisomineni23@gmail.com>
2025-07-10 22:34:15 +0530

    Create q-13insertintobst.cpp

---
+++ week-4/q-13insertintobst.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==nullptr) return new TreeNode(val);
+        if(root->val<val) root->right = insertIntoBST(root->right,val);
+        if(root->val>val) root->left= insertIntoBST(root->left,val);
+        return root;
+    }
+};
